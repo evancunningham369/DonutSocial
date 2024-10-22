0b4e1a9cf6385f27f28c9590ab055abdd52395bc
evancunningham369 <evancunningham369@yahoo.com>
2024-10-22 09:35:24 -0500

    retrieve user when deserializing user

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import { pool } from './database.js';
 import { google_strategy, login_account } from "../controllers/authentication-controller.js"
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
@@ -11,18 +12,25 @@ const googleStrategyConfig = {
 };
 
 passport.use(new GoogleStrategy(googleStrategyConfig, google_strategy));
-passport.use(new LocalStrategy({usernameField: 'username', passwordField: 'password'}, login_account));
+passport.use(new LocalStrategy(login_account));
 
 passport.serializeUser((user, done) => {
+    
     done(null, user.user_id);
-  });
-  
-passport.deserializeUser(async (user, done) => {
-try {
-    done(null, user.userId);
-} catch (err) {
-    done(err);
-}
+});
+
+passport.deserializeUser(async (user_id, done) => {
+    try {
+        const result = await pool.query('SELECT * FROM account WHERE user_id = $1', [user_id]);
+        if(result.rowCount === 0){
+            return done(null, false);
+        }
+        const user = result.rows[0];
+        console.log('Deserialize:', user);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
 });
 
 export default passport;
