5ef4294bdd7fca0f14aa5d0d27a4f6881c84c6b9
evancunningham369 <evancunningham369@yahoo.com>
2024-10-24 03:55:39 -0500

    add Logout functionality

--- vite-donut-social-app/src/components/useLogout.jsx
+++ vite-donut-social-app/src/components/useLogout.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import { logout } from "../../api/account";
 
 function Logout() {
+    const navigate = useNavigate();
+
+    const handleLogout = async() => {
+        const response = await logout();
+
+        if(response.ok){
+            sessionStorage.clear();
+            navigate('/');
+        }
+    }
 
     return(
         <header>
             <div className="logout">
-                <button className="btn btn-primary" onClick={logout}>Logout</button>
+                <button className="btn btn-primary" onClick={handleLogout}>Logout</button>
             </div>
         </header>
     )
