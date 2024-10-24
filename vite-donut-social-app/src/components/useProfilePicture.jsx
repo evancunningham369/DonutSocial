9cb5a3cbe3133be003bb4822e5934fb19a89fa30
evancunningham369 <evancunningham369@yahoo.com>
2024-10-24 03:57:47 -0500

    add function to retrieve profile picture

--- vite-donut-social-app/src/components/useProfilePicture.jsx
+++ vite-donut-social-app/src/components/useProfilePicture.jsx
@@ -3,24 +3,30 @@ import { get_profile_picture } from '../../api/account.js';
 import { useEffect, useState } from 'react';
 
 function useProfilePicture(){
+
+    const loggedInUserId = sessionStorage.getItem('userId');
     const [profilePicture, setProfilePicture] = useState(donut);
 
     useEffect(() => {
-        async function getProfilePicture() {
-    
+        const getProfilePicture = async(loggedInUserId) => {
+            
             try {
-                const url = await get_profile_picture(loggedInUserId);
-                if (url == null) throw TypeError
-        
-                setProfilePicture(url);
+                let url = await get_profile_picture(loggedInUserId);
+                if(url.error){
+                    url = donut;
+                }
+                if(JSON.stringify(url) !== JSON.stringify(profilePicture)){
+                    setProfilePicture(donut);
+                }
             } catch (error) {
                 setProfilePicture(donut);
             }
         }
         getProfilePicture();
-    })
+    }, [loggedInUserId])
 
     return { profilePicture };
+
 }
 
 export default useProfilePicture;
