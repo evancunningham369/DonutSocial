c197a203f9af6ca271d64d68f381971342d53ea6
evancunningham369 <evancunningham369@yahoo.com>
2024-10-25 01:40:04 -0500

    add deletePost function

--- vite-donut-social-app/src/components/Posts.jsx
+++ vite-donut-social-app/src/components/Posts.jsx
@@ -3,16 +3,16 @@ import Post from './Post.jsx'
 import usePosts from "./usePosts.jsx";
 
 function Posts({postType}) {
-    //console.log('fetching posts');
     const [posts, setPosts] = useState([]);
     let loggedInUserId = sessionStorage.getItem('userId');
-    const { getPost } = usePosts(loggedInUserId);
+    const { getPost, deletePost } = usePosts(loggedInUserId, posts, setPosts);
 
     const fetchPosts = async() => {
         try {
             const allPosts = await getPost(postType);
             if(JSON.stringify(posts) !== JSON.stringify(allPosts)){
                 setPosts(allPosts);
+                
             }
         } catch (error) {
             console.error("Error fetching posts:", error);
@@ -27,7 +27,7 @@ function Posts({postType}) {
         <div className="posts">
                 {posts.length === 0 ?
                     <h1>No {postType} posts</h1> :
-                    posts.map((post) => <Post key={post.post_id} userIdPoster={post.user_id} initialPost={post} />)}
+                    posts.map((post) => <Post key={post.post_id} userIdPoster={post.user_id} initialPost={post} deletePost={deletePost} />)}
             </div>
     )
 }
