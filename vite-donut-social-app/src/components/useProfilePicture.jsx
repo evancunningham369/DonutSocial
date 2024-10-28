6c5fb8ae5926d0697295366473b88caa6915fad1
evancunningham369 <evancunningham369@yahoo.com>
2024-10-28 08:01:15 -0500

    add condition to check for userId existence

--- vite-donut-social-app/src/components/useProfilePicture.jsx
+++ vite-donut-social-app/src/components/useProfilePicture.jsx
@@ -3,9 +3,9 @@ import { get_profile_picture } from '../../api/account.js';
 import { useEffect, useState } from 'react';
 
 function useProfilePicture(userId){
-    console.log(userId);
     
     const [profilePicture, setProfilePicture] = useState(donut);
+    if(userId){
         const getProfilePicture = async() => {
             try {
                 let url = await get_profile_picture(userId);
@@ -14,10 +14,11 @@ function useProfilePicture(userId){
                     setProfilePicture('http://localhost:3001/'+ url.replace(/\\/g, '/'));
                 }
             } catch (error) {
+                setProfilePicture(donut);
             }
-        }
+        };
         getProfilePicture();
-
+    }
     return { profilePicture, setProfilePicture };
 
 }
