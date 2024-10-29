267d4bfd3e6499439aa9473814c0d8d21496548d
evancunningham369 <evancunningham369@yahoo.com>
2024-10-29 10:32:42 -0500

    reworked SQL query to reflect followers table addition

--- server/controllers/account-controller.js
+++ server/controllers/account-controller.js
@@ -53,25 +53,40 @@ export const delete_profile_picture = async(req, res) => {
 
 export const is_user_following = async(req, res) => {
     try {
-        const { userId, profileId } = req.query;
-        const followed = await pool.query('SELECT followed_users FROM account WHERE user_id=$1', [userId]);
-        const followed_users = followed.rows[0].followed_users;
-        const response = await pool.query('SELECT $1 = ANY($2) as is_following', [profileId, followed_users]);
-        res.json(response.rows[0]);
+        const { followerId, followedId } = req.params;
+        const result = await pool.query(`
+            SELECT EXISTS (
+                SELECT 1
+                FROM followers
+                WHERE follower_id = $1 AND followed_id = $2)`,
+        [followerId, followedId]);
+
+        res.status(200).json({follow: result.rows[0].exists});
     } catch (error) {
-        res.json(error.message);
+        res.status(500).json(error.message);
     }
 }
 
 export const get_profile_info = async(req, res) => {
     try {
         const { userId } = req.params;
-        const following = await pool.query('SELECT COALESCE(array_length(followed_users, 1), 0) as followingCount FROM account WHERE user_id=$1', [userId]);
-        const follower = await pool.query('SELECT COALESCE(array_length(followers, 1), 0) as followerCount FROM account WHERE user_id=$1', [userId]);
-        const profileInfo = {followingCount: following.rows[0].followingcount, followerCount: follower.rows[0].followercount}
-        res.json(profileInfo);
+        
+        let result = await pool.query(`
+            SELECT COUNT(followed_id) AS following_count
+            FROM followers
+            WHERE follower_id = $1`,
+        [userId]);
+        const followingCount = result.rows[0].following_count;
+        
+        result = await pool.query(`
+            SELECT COUNT(follower_id) AS follower_count
+            FROM followers
+            WHERE followed_id = $1`,
+        [userId]);
+        const followerCount = result.rows[0].follower_count;
+
+        res.status(200).json({followerCount: followerCount, followingCount: followingCount});
     } catch (error) {
-        console.log(error);
-        res.json(error.message);
+        res.status(500).json(error.message);
     }
 }
